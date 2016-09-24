1d85504197e1f3c4ed67f59f38d430625c60cc8c
JoelACR <jchava1@students.towson.edu>
2016-09-24 15:54:59 -0400

    Task 5 completed

--- src/main/scala/edu/towson/cosc/cosc455/jchava1/lab4/SyntaxAnalyzer.scala
+++ src/main/scala/edu/towson/cosc/cosc455/jchava1/lab4/SyntaxAnalyzer.scala
@@ -3,31 +3,36 @@ package edu.towson.cosc.cosc455.jchava1.lab4
 class SyntaxAnalyzer {
 
   // For ease, store the terminal literals in a List
-  val ARTICLES : List[String] = List("teh", "a")
-  val VERBS : List[String] = List("ates", "lovez", "hatez")
-  val NOUNS : List[String] = List("kat", "dawg", "rat")
-  val ADJ : List[String] = List("fat", "hungry", "happy", "mean")
+  val ARTICLES: List[String] = List("teh", "a")
+  val VERBS: List[String] = List("ates", "lovez", "hatez")
+  val NOUNS: List[String] = List("kat", "dawg", "rat")
+  val ADJ: List[String] = List("fat", "hungry", "happy", "mean")
+  val ADV: List[String] = List("accidently", "quickly", "secretly")
 
   // Flag for errors and helper methods
-  var errorFound : Boolean = false
+  var errorFound: Boolean = false
+
   def setError() = errorFound = true
+
   def resetError() = errorFound = false
-  def getError : Boolean = errorFound
+
+  def getError: Boolean = errorFound
 
 
   // This method implements the BNF rule for a sentence <S> ::= <NP> <V> <NP>
   def Sentence() = {
     resetError()
     if (!errorFound) NounPhrase()
-    if(!errorFound) Verb()
-    if(!errorFound) NounPhrase()
+    if (!errorFound) Adv()
+    if (!errorFound) Verb()
+    if (!errorFound) NounPhrase()
   }
 
   // This method implements the BNF rule for a noun phrase <NP> ::= <A> <Adj> <N>
   def NounPhrase() = {
-    if(!errorFound) Article()
-    if(!errorFound) Adj()
-    if(!errorFound) Noun()
+    if (!errorFound) Article()
+    if (!errorFound) Adj()
+    if (!errorFound) Noun()
 
   }
 
@@ -46,9 +51,9 @@ class SyntaxAnalyzer {
     if (NOUNS contains Compiler.currentToken)
       Compiler.Scanner.getNextToken()
     else {
-        println("SYNTAX ERROR - A noun was expected when '" + Compiler.currentToken + "' was found.")
-        setError()
-      }
+      println("SYNTAX ERROR - A noun was expected when '" + Compiler.currentToken + "' was found.")
+      setError()
+    }
   }
 
   // This method implements the BNF rule for an article <N> ::= teh | a
@@ -56,12 +61,12 @@ class SyntaxAnalyzer {
     if (ARTICLES contains Compiler.currentToken)
       Compiler.Scanner.getNextToken()
     else {
-        println("SYNTAX ERROR - An article was expected when '" + Compiler.currentToken + "' was found.")
-        setError()
-      }
+      println("SYNTAX ERROR - An article was expected when '" + Compiler.currentToken + "' was found.")
+      setError()
+    }
   }
 
-  //Method impletments the BNF rule for an adjective
+  //Method implements the BNF rule for an adjective
   def Adj() = {
     if (ADJ contains Compiler.currentToken) {
       Compiler.Scanner.getNextToken()
@@ -69,6 +74,13 @@ class SyntaxAnalyzer {
     else {
       println("Syntax ERROR - An adjective was expected when '" + Compiler.currentToken + "' was found.")
       setError()
+    }
+  }
+
+  //Method implements the BNF rule for an adverb
+  def Adv() = {
+    if (ADV contains Compiler.currentToken) {
+      Compiler.Scanner.getNextToken()
     }
   }
 }
